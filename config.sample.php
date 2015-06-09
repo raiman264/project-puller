6e3f95ed508d387bc6eba0cea0d100454e09ed0b
Braulio Ruiz <braulior@vsisoft.com>
2015-06-09 17:16:42 -0500

    cloning new repo working

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+define("GITHUB_SECRET","123XXXxxxx"); //set this on your github web hook
+
+define("BASE_DIRECTORY","/var/www/testfield/"); //base directory where the proyects will be cloned
+
+define("SSH_KEY_FILE", ".ssh/id_rsa"); //relative path to this proyect
---
+++ hook-me.php
@@ -0,0 +1,71 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include "config.php";
+
+define("CORE_PATH",realpath(dirname(__FILE__)));
+
+$request_body = file_get_contents('php://input');
+
+if( validate_request($request_body) ) {
+   $json_request = json_decode($request_body);
+   #$star = "----------------------\n".print_r($json_request,1)."\n\n";
+   #file_put_contents("log.txt",$star,FILE_APPEND);
+
+   echo CORE_PATH."\n";
+
+   $ssh_key = "-i ".CORE_PATH."/".SSH_KEY_FILE;
+
+   if( !file_exists(BASE_DIRECTORY.$json_request->repository->name) ){
+      echo "proyect doesnt exist, cloning repo\n";
+
+      chdir(BASE_DIRECTORY);
+      $output = array();
+      #exec("ssh-keygen -R github.com", $output);
+
+      $command = CORE_PATH."/git.sh $ssh_key clone ".escapeshellcmd($json_request->repository->ssh_url);
+      echo "\n".$command."\n";
+      exec ( $command, $output, $return_var );
+
+      print_r($output);
+
+      var_dump("result,", $return_var);
+      
+      if($return_var){
+         echo "\nsuccess";
+      }
+   } else {
+       echo "proyect exist, pulling data\n";
+
+      chdir(BASE_DIRECTORY.$json_request->repository->name);
+      $output = array();
+      #exec("ssh-keygen -R github.com", $output);
+
+      
+      $command = CORE_PATH."/git.sh $ssh_key pull");
+      echo "\n".$command."\n";
+      exec ( $command, $output, $return_var );
+
+      print_r($output);
+
+      var_dump("result,", $return_var);
+      
+      if($return_var){
+         echo "\nsuccess";
+      }
+   }
+
+   echo "ok";
+} else {
+   http_response_code(401);
+   die("401 verification failed");
+}
+
+function validate_request($payload) {
+   return true;
+   $algo = "sha1";
+   $signature = hash_hmac ( $algo , $payload , GITHUB_SECRET);
+
+   return isset($_SERVER['HTTP_X_HUB_SIGNATURE']) && $_SERVER['HTTP_X_HUB_SIGNATURE'] == $algo."=".$signature;
+}
